7aaaa5f2b9ad18b9babefdd96633802e7161a3e1
hangiljun <6332159@gmail.com>
2025-11-05 22:27:39 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// GitHub Pages용 리포 이름
-const REPO = "myplan";
-
-// Vercel 환경에선 base="/" , GitHub Pages에선 "/<REPO>/"
-const isVercel = process.env.VERCEL === "1" || process.env.VERCEL === "true";
-
 export default defineConfig({
   plugins: [react()],
-  base: isVercel ? "/" : `/${REPO}/`
+  base: "/", // Vercel에서는 루트로 배포
 });
